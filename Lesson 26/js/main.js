f46a6da42468414b3174c425d4324fd4cbd453be
YllAbazii <dielli.abazi@gmail.com>
2025-05-10 14:47:19 +0200

    Lesson 26

---
+++ Lesson 26/js/main.js
@@ -0,0 +1,105 @@
+console.log("hello there")
+
+var x=99;
+console.log(x)
+
+var y=999;
+console.log(x+y)
+
+var a=90
+
+var b=80
+
+var c=a +b
+
+console.log(c)
+
+var w=6
+
+var s=30 
+
+var v=50
+
+console.log(w*w+s/v)
+
+n=10 
+m=3 
+rezultati=(n%m)
+
+console.log(rezultati)
+
+console.log(133%13)
+
+nr=100
+nr--;
+nr--;
+nr--;
+console.log(nr)
+
+nr2=300
+nr2++;
+nr2++;
+nr2++;
+
+console.log(nr2)
+
+var h=20
+
+h+=13
+
+console.log(h)
+
+var t=20
+
+t-=7
+
+console.log(t)
+
+var p=20
+
+p*=7
+
+console.log(p)
+
+var o=20
+
+o/=7
+
+console.log(o)
+
+var g=7
+var f=7 
+
+console.log(g==f)
+
+var u=9
+var l=8  
+
+console.log(u==l)
+
+var j=8
+var z=8
+console.log(j!=z)
+
+
+var nr9=60000
+var nr5=90000000000
+
+console.log(nr9>nr5)
+
+console.log(nr9<nr5)
+
+var age=46
+
+console.log(age>24 && age<69)
+
+var kr=7
+var kl=9
+
+console.log(kr==7 || kl==9)
+
+var firstname="Yll"
+var lastname="Abazi"
+
+console.log(firstname+" "+lastname)
+    
