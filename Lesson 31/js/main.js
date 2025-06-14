17d23015b88c866bcb8abada787296fca6fc1027
YllAbazii <dielli.abazi@gmail.com>
2025-06-14 13:44:08 +0200

    lesson 31

---
+++ Lesson 31/js/main.js
@@ -0,0 +1,41 @@
+var frutat =['molla','dardha','qershia']
+
+for(var i=0; i<frutat.length; i++){
+    console.log(frutat[i])
+}
+
+
+console.log('----------')
+
+var e = 0;
+do{
+    e = e+1;
+   console.log(e); 
+
+}while(e<5)
+
+
+console.log('----------')
+
+var j = 0;
+while(j<5){
+   console.log(j);
+    j=j+1;
+}
+
+var person={Firstname:'John',Lastname:'Doe',Age:25}
+
+for(z in person){
+    console.log(person[z])
+}
+
+for(z in frutat){
+    console.log(frutat[z])
+}
+
+var teksti='javascript'
+
+for(l of teksti){
+    console.log(l)
+
+}
